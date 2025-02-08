eb83086d5cd16a423c2c3abde9ceafb4a20ab2fc
Cyril Anisimov <CyAn84@gmail.com>
2025-02-08 20:30:54 +0100

    apply format to file

--- client/core/ipcclient.cpp
+++ client/core/ipcclient.cpp
@@ -3,17 +3,16 @@
 #include <chrono>
 #include <thread>
 
-
 IpcClient *IpcClient::m_instance = nullptr;
 
 IpcClient::IpcClient(QObject *parent) : QObject(parent)
 {
-
 }
 
 IpcClient::~IpcClient()
 {
-    if (m_localSocket) m_localSocket->close();
+    if (m_localSocket)
+        m_localSocket->close();
 }
 
 bool IpcClient::isSocketConnected() const
@@ -28,13 +27,15 @@ IpcClient *IpcClient::Instance()
 
 QSharedPointer<IpcInterfaceReplica> IpcClient::Interface()
 {
-    if (!Instance()) return nullptr;
+    if (!Instance())
+        return nullptr;
     return Instance()->m_ipcClient;
 }
 
 QSharedPointer<IpcProcessTun2SocksReplica> IpcClient::InterfaceTun2Socks()
 {
-    if (!Instance()) return nullptr;
+    if (!Instance())
+        return nullptr;
     return Instance()->m_Tun2SocksClient;
 }
 
@@ -78,9 +79,8 @@ bool IpcClient::init(IpcClient *instance)
         }
     });
 
-    connect(Instance()->m_localSocket, &QLocalSocket::disconnected, [instance](){
-        instance->m_isSocketConnected = false;
-    });
+    connect(Instance()->m_localSocket, &QLocalSocket::disconnected,
+            [instance]() { instance->m_isSocketConnected = false; });
 
     Instance()->m_localSocket->connectToServer(amnezia::getIpcServiceUrl());
     Instance()->m_localSocket->waitForConnected();
@@ -97,7 +97,7 @@ bool IpcClient::init(IpcClient *instance)
 
 QSharedPointer<PrivilegedProcess> IpcClient::CreatePrivilegedProcess()
 {
-    if (! Instance()->m_ipcClient || ! Instance()->m_ipcClient->isReplicaValid()) {
+    if (!Instance()->m_ipcClient || !Instance()->m_ipcClient->isReplicaValid()) {
         qWarning() << "IpcClient::createPrivilegedProcess : IpcClient IpcClient replica is not valid";
         return nullptr;
     }
@@ -120,18 +120,15 @@ QSharedPointer<PrivilegedProcess> IpcClient::CreatePrivilegedProcess()
         pd->ipcProcess.reset(priv);
         if (!pd->ipcProcess) {
             qWarning() << "Acquire PrivilegedProcess failed";
-        }
-        else {
+        } else {
             pd->ipcProcess->waitForSource(1000);
             if (!pd->ipcProcess->isReplicaValid()) {
                 qWarning() << "PrivilegedProcess replica is not connected!";
             }
 
-            QObject::connect(pd->ipcProcess.data(), &PrivilegedProcess::destroyed, pd->ipcProcess.data(), [pd](){
-                pd->replicaNode->deleteLater();
-            });
+            QObject::connect(pd->ipcProcess.data(), &PrivilegedProcess::destroyed, pd->ipcProcess.data(),
+                             [pd]() { pd->replicaNode->deleteLater(); });
         }
-
     });
     pd->localSocket->connectToServer(amnezia::getIpcProcessUrl(pid));
     pd->localSocket->waitForConnected();
@@ -139,5 +136,3 @@ QSharedPointer<PrivilegedProcess> IpcClient::CreatePrivilegedProcess()
     auto processReplica = QSharedPointer<PrivilegedProcess>(pd->ipcProcess);
     return processReplica;
 }
-
-
